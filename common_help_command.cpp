d43ca2674cb17c34771e53d1a92f9e5bf80cb321
tszemzo <szemzotomas@gmail.com>
2020-06-08 00:25:24 -0300

    mini fixes

--- common_help_command.cpp
+++ common_help_command.cpp
@@ -5,11 +5,11 @@ Help::Help(Protocol *protocol) {
 }
 
 void Help::execute() {
-    std::string msg_1 = "Comandos válidos:\n\t​AYUDA: despliega la ";
-    std::string msg_2 = "lista de comandos válidos​\n\t​RENDIRSE: ";
-    std::string msg_3 = "pierde el juego automáticamente​\n\t​XXX: ";
+    std::string msg_1 = "Comandos válidos\n\tAYUDA: despliega la ";
+    std::string msg_2 = "lista de comandos válidos\n\tRENDIRSE: ";
+    std::string msg_3 = "pierde el juego automáticamente\n\tXXX: ";
     std::string msg_4 = "Número de 3 cifras a ser enviado al servidor ";
-    std::string msg_5 = "para adivinar el número secreto";
+    std::string msg_5 = "para adivinar el múmero secreto";
     std::string msg = msg_1 + msg_2 + msg_3 + msg_4 + msg_5;
     socket_protocol->send_string(msg);
 }
